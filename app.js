c8d9f973ec42cbdaafdd1ff8bc4196e3bb7d7076
David Bilson <85068757+dlbi@users.noreply.github.com>
2022-08-04 05:57:26 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let register = document.getElementById('register');
+let login = document.getElementById('loginUser');
+let registerBtn = document.getElementById('registerBtn');
+let loginBtn = document.getElementById('loginBtn');
+
+register.style.display = 'none';
+
+registerBtn.addEventListener('click', function() {
+    login.style.display = 'none';
+    register.style.display = 'block';
+});
+
+loginBtn.addEventListener('click', function() {
+    register.style.display = 'none';
+    login.style.display = 'block';
+});
